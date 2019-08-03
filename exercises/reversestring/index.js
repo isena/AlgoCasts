88d0c0b5dd1149a0c8b65b39ee6ca4ad3e5de481
Isaac Sena <isaac.sena@soupi.com.br>
2019-08-03 15:14:31 -0300

    reversestring answered

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,54 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// // solution 3.1
+// const reverse = (str) => 
+//   str.split('').reduce(
+//     (total, currentValue) => currentValue + total, ''
+//   )
+
+// solution 3
+const reverse = (str) => {
+  debugger;
+  return str.split('').reduce(function(total, currentValue) {
+    // console.log(`${total} + ${currentValue}`)
+    total = currentValue + total
+    return total
+  }, '')
+}
+
+reverse('asdf');
+
+// // solution 2
+// const reverse = (str) => {
+//   let reversed = ''
+
+//   for (character of str) {
+//     reversed = character + reversed
+//   }
+
+//   return reversed
+// }
+
+// // solution 1.1
+// const reverse = (_) => _.split('').reverse().join('')
+
+// // solution 1
+// function reverse(str) {
+//   const arr = str.split('')
+//   arr.reverse();
+//   return arr.join('')
+// }
+
+// my solution
+// function reverse(str) {
+//   let newString = ''
+  
+//   for (var i = str.length -1; i >= 0; i--) {
+//     newString += str[i];
+//   }
+
+//   return newString;
+// }
 
 module.exports = reverse;
