fe92f86c1d7a3b6e590285d1022afa79ede315e3
Isaac Sena <isaac.sena@soupi.com.br>
2019-08-05 22:21:12 -0300

    Section 9: Anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,51 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// Solution #2
+const anagrams = (stringA, stringB) => cleaning(stringA) === cleaning(stringB)
+
+const cleaning = _ => _.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('')
+
+// // Solution #1
+
+// const cleaning = _ => _.replace(/[^\w]/g, '').toLowerCase()
+
+// const charsMap = _ => {
+//   const charsMap = {}
+
+//   for (char of cleaning(_)) {
+//     charsMap[char] = charsMap[char] + 1 || 1
+//   }
+
+//   return charsMap
+// }
+
+// const keysLength = _ => Object.keys(_).length
+
+// const anagrams = (stringA, stringB) => {
+//   const stringAMap = charsMap(stringA)
+//   const stringBMap = charsMap(stringB)
+
+//   if (keysLength(stringAMap) !== keysLength(stringBMap)) return false
+
+//   for (let char in stringAMap) {
+//     if (stringAMap[char] !== stringBMap[char]) return false
+//   }
+
+//   return true
+// }
+
+  // My Solution
+  // let isAnagram = true
+
+  // Object.entries(stringAMap).every(([key, value]) => {
+  //   if (stringBMap[key] !== value) {
+  //     isAnagram = false
+  //     return
+  //   }
+  // })
+
+  // return isAnagram
+// }
 
 module.exports = anagrams;
