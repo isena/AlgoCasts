924b6fed661a27b91f75b213ab37dca01dd5839b
Isaac Sena <isaac.sena@gmail.com>
2019-08-26 22:53:40 -0300

    Section 16: Runtime Complexity in Practice - Fibonacci

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,72 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// function fib(n) {}
+
+// ###################
+// # Course Solution #
+// ###################
+const memoize = fn => {
+  const cache = {}
+
+  return function(...args) {
+    if (cache[args])
+      return cache[args]
+
+    const result = fn.apply(this, args)
+    cache[args] = result    
+
+    return result
+  }
+}
+
+const slowFib = n => {
+  if (n < 2)
+    return n
+  
+  return fib(n - 1) + fib(n - 2)
+}
+
+const fib = memoize(slowFib)
+
+// // ###############
+// // # Memoization #
+// // ###############
+// const memoization = []
+
+// const fib = n => {
+//   if (n < 2)
+//     return n
+  
+//   if (memoization[n])
+//     return memoization[n]
+  
+//   memoization[n] = fib(n - 1) + fib(n - 2)
+
+//   return memoization[n]
+// }
+
+// // #############
+// // # Recursive #
+// // #############
+// const fib = n => {
+//   if (n < 2)
+//     return n
+
+//   return fib(n - 1) + fib(n - 2)
+// }
+
+// // #############
+// // # Iterative #
+// // #############
+// const fib = n => {
+//   const result = []
+
+//   for (let i = 0; i <= n; i++) {
+//     result[i] = i === 0 || i === 1 ? i : result[i - 1] + result[i - 2]
+//   }
+
+//   return result[n]
+// }
 
 module.exports = fib;
