d214f06898c3003b48b2db4116f0035bbb1df015
Isaac Sena <isaac.sena@gmail.com>
2019-09-27 21:35:45 -0300

    LinkedList: 86. Adding InsertLast

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -76,6 +76,18 @@ class LinkedList {
 
     previous.next = null
   }
+
+  insertLast(data) {
+    const node = new Node(data)
+    const last = this.getLast()
+
+    if (last) {
+      last.next = node
+    } else {
+      this.head = node
+      return
+    }
+  }
 }
 
 module.exports = { Node, LinkedList }
