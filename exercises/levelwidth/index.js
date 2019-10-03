4c662a6dadd639def561c50c24323392bca2dd9a
Isaac Sena <isaac.sena@gmail.com>
2019-10-03 09:22:26 -0300

    Section 26: Tree Width with Level Width

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,24 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  const TOKEN = 's'
+  const counters = [0]
+  const arr = [root, 's']
 
-module.exports = levelWidth;
+  while (arr.length > 1) {
+      const node = arr.shift()
+
+      if (node === TOKEN) {
+          counters.push(0)
+          arr.push(TOKEN)
+      } else {
+          arr.push(...node.children)
+          counters[counters.length - 1]++
+      }
+  }
+
+  return counters
+}
+
+module.exports = levelWidth
