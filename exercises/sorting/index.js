6dbdded38958d1ea32770a27d267365cdb3be54b
Isaac Sena <isaac.sena@gmail.com>
2019-10-11 12:11:07 -0300

    Section 32: Sort By Selection

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -16,7 +16,22 @@ function bubbleSort(arr) {
 }
 
 function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let min = i
+    for (let j = i+1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+        min = j
+      }
+    }
 
+    if (min !== i) {
+      let lesser = arr[min]
+      arr[min] = arr[i]
+      arr[i] = lesser
+    }
+  }
+
+  return arr
 }
 
 function mergeSort(arr) {
